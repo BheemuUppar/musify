d2c8f577828d40f911460b9f398a173087a8e165
bheemuk123 <bheemuk123@gmail.com>
2024-04-16 21:31:53 +0530

    view album song card

--- client/src/components/ViewAlbum.tsx
+++ client/src/components/ViewAlbum.tsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { environment } from "../assets/environment";
 import ListHeader from "./ListHeader";
+import PlayButton from "./PlayButton";
+import AlbumSongCard from "./AlbumSongCard";
 
 const ViewAlbum = React.memo(() => {
-    const [album, setAlbum] = useState({});
+  const [album, setAlbum]:any = useState({});
   const params = useParams();
 
   useEffect(() => {
@@ -13,15 +15,21 @@ const ViewAlbum = React.memo(() => {
       .get(`${environment.searchUrl}/albumsById/${params.id}`)
       .then((data: any) => {
         console.log(data.data.data);
-        setAlbum(data.data.data)
+        setAlbum(data.data.data);
       });
   }, []);
 
-  return <>
-  <div>
-  <ListHeader list={album}/>
-  </div>
-  </>;
+  return (
+    <>
+      <div>
+        <ListHeader list={album} />
+        <PlayButton/>
+        {album && album.songs && album.songs.map((song : any)=>{
+          return <AlbumSongCard key={song.id} song={song}/>
+        })}
+      </div>
+    </>
+  );
 });
 
 export default ViewAlbum;
