4bed1dc139c4aa8f80352a239d84005f76f87c9d
bheemuk123 <bheemuk123@gmail.com>
2024-05-02 22:52:13 +0530

    minor changes

--- client/src/components/Player/Player.tsx
+++ client/src/components/Player/Player.tsx
@@ -67,7 +67,16 @@ function Player() {
     }
   }
   let toggleFullScreenMode = () => {
-    if (/Mobi|Android/i.test(navigator.userAgent) || window.innerWidth <= 768) {
+    if (
+      navigator.userAgent.match(/Android/i)
+      ||  navigator.userAgent.match(/webOS/i)
+      ||  navigator.userAgent.match(/iPhone/i)
+      ||  navigator.userAgent.match(/iPad/i)
+      ||  navigator.userAgent.match(/iPod/i)
+      ||  navigator.userAgent.match(/BlackBerry/i)
+      ||  navigator.userAgent.match(/Windows Phone/i)
+    || window.innerWidth < 768
+    ) {
       showNotification({severity:"error", message:"this feature is not available in mobile device"})
       
     }else{
