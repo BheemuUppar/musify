57895c7e26112e9850a8467248f6b7f57e33f605
bheemuk123 <bheemuk123@gmail.com>
2024-05-04 12:07:28 +0530

    search page playlist and  album places changed

--- client/src/components/Main/SearchPage.tsx
+++ client/src/components/Main/SearchPage.tsx
@@ -12,24 +12,24 @@ import { Playlist } from "../../types/Playlist";
 import { Album } from "../../types/album";
 import { Song } from "../../types/song";
 
-const searchUrl = import.meta.env.VITE_SEARCH_URL
-
+const searchUrl = import.meta.env.VITE_SEARCH_URL;
 
 function SearchPage() {
   const searchText = useRecoilValue(searchTextAtom);
   const [searchResults, setSearchResults] =
-    useRecoilState(searchResultsAtom);
-  const [currentSongList, setCurrentSongList] = useRecoilState(currentSongsListAtom);
+    useRecoilState<SearchResults>(searchResultsAtom);
+  const [currentSongList, setCurrentSongList] =
+    useRecoilState(currentSongsListAtom);
 
   useEffect(() => {
     searchHandler();
   }, [searchText]);
   async function searchHandler() {
-    let obj: SearchResults= {
+    let obj: SearchResults = {
       albums: [],
       playlists: [],
       songs: [],
-      artists: []
+      artists: [],
     };
     if (searchText) {
       obj["albums"] = await fetchAlbums();
@@ -42,17 +42,14 @@ function SearchPage() {
   }
 
   async function fetchAlbums() {
-    let data = await axios.get(
-      `${searchUrl}/albums/${searchText}`,
-      {
-        headers: {
-          Authorization: localStorage.getItem("token"),
-        },
-      }
-    );
+    let data = await axios.get(`${searchUrl}/albums/${searchText}`, {
+      headers: {
+        Authorization: localStorage.getItem("token"),
+      },
+    });
     return data.data;
   }
-  
+
   async function fetchSongs() {
     let data = await axios.get(`${searchUrl}/songs/${searchText}`, {
       headers: {
@@ -62,25 +59,19 @@ function SearchPage() {
     return data.data;
   }
   async function fetchPlaylist() {
-    let data = await axios.get(
-      `${searchUrl}/playlist/${searchText}`,
-      {
-        headers: {
-          Authorization: localStorage.getItem("token"),
-        },
-      }
-    );
+    let data = await axios.get(`${searchUrl}/playlist/${searchText}`, {
+      headers: {
+        Authorization: localStorage.getItem("token"),
+      },
+    });
     return data.data;
   }
   async function fetchArtists() {
-    let data = await axios.get(
-      `${searchUrl}/artists/${searchText}`,
-      {
-        headers: {
-          Authorization: localStorage.getItem("token"),
-        },
-      }
-    );
+    let data = await axios.get(`${searchUrl}/artists/${searchText}`, {
+      headers: {
+        Authorization: localStorage.getItem("token"),
+      },
+    });
     return data.data;
   }
 
@@ -95,10 +86,7 @@ function SearchPage() {
       <div className="grid grid-cols-3 gap-2">
         {searchResults && searchResults.albums.length > 0 && (
           <div className="col-span-1 ">
-            <TopResultCard
-             
-              album={searchResults && searchResults.albums[0] }
-            />
+            <TopResultCard album={searchResults && searchResults.albums[0]} />
           </div>
         )}
         {searchResults && searchResults.songs.length > 0 && (
@@ -119,29 +107,59 @@ function SearchPage() {
         )}
       </div>
 
+      <DisplayAlbums searchResults={searchResults} />
+      <DisplayPlaylists searchResults={searchResults} />
+
+
+      <CollaborationWrapper />
+    </>
+  );
+}
+
+function DisplayPlaylists({ searchResults }: { searchResults: SearchResults }) {
+  return (
+    <>
       <div className="my-2 text-dark-600 dark:text-white">
         <h2 className="text-xl  my-3">Playlists</h2>
         <div className="flex flex-wrap gap-4">
-          { searchResults && searchResults.playlists &&
+          {searchResults &&
+            searchResults.playlists &&
             searchResults.playlists.map((playlist: Playlist) => {
-              return <PlaylistCard playlist={playlist} key={playlist.id} path={'/home/playlist/' + playlist.id}/>;
+              return (
+                <PlaylistCard
+                  playlist={playlist}
+                  key={playlist.id}
+                  path={"/home/playlist/" + playlist.id}
+                />
+              );
             })}
-              {!searchResults && <h1>No playlist Available</h1>}
+          {!searchResults && <h1>No playlist Available</h1>}
         </div>
       </div>
+    </>
+  );
+}
+
+function DisplayAlbums({ searchResults }: { searchResults: SearchResults }) {
+  return (
+    <>
       {/* Albums  */}
       <div className="my-6 text-dark-600 dark:text-white">
         <h2 className="text-xl my-3">Albums</h2>
         <div className="flex flex-wrap gap-4">
-          {searchResults && searchResults.albums &&
-            searchResults.albums.map((album:Album) => {
-              return <PlaylistCard playlist={album} path={'/home/album/'+album.id}/>
+          {searchResults &&
+            searchResults.albums &&
+            searchResults.albums.map((album: Album) => {
+              return (
+                <PlaylistCard
+                  playlist={album}
+                  path={"/home/album/" + album.id}
+                />
+              );
             })}
-            {!searchResults && <h1>No Albums Available</h1>}
+          {!searchResults && <h1>No Albums Available</h1>}
         </div>
       </div>
-
-      <CollaborationWrapper />
     </>
   );
 }
